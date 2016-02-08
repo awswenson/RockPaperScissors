2b6789c0180580a03ba86c8936f55771f529c2e5
Alex Swenson <swensona@charter.net>
2016-02-08 14:27:59 -0600

    Finished part 1 (added a play button and implemented the button click to start a new game play activity.

--- app/src/main/java/com/cs407_android/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/cs407_android/rockpaperscissors/MainActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.xgc1986.ripplebutton.widget.RippleButton;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
+        btnPlay = (Button)findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                playPressed(view);
+            }
+        });
     }
 
     @Override
@@ -31,7 +42,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void playPressed(View view) {
-        //TODO: Put to start game play activity
+        // Start up the game play activity
+        Intent intent = new Intent(this, PlayActivity.class);
+        startActivity(intent);
     }
 
     @Override
